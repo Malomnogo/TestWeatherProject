2723c9cc40b7d986f8409054469b9709e4d4c68f
Eugene <auricamercer@gmail.com>
2025-12-02 15:01:24 +0300

    feat: domain

---
+++ app/src/main/java/com/malomnogo/testweatherproject/weather/domain/WeatherDomain.kt
@@ -0,0 +1,34 @@
+package com.malomnogo.testweatherproject.weather.domain
+
+interface WeatherDomain {
+
+    fun <T : Any> map(mapper: Mapper<T>): T
+
+    interface Mapper<T : Any> {
+
+        fun mapSuccess(
+            city: String,
+            temperature: Double
+        ): T
+
+        fun mapError(message: String): T
+    }
+
+    data class Success(
+        private val city: String,
+        private val temperature: Double
+    ) : WeatherDomain {
+
+        override fun <T : Any> map(mapper: Mapper<T>) = mapper.mapSuccess(
+            city = city,
+            temperature = temperature
+        )
+    }
+
+    data class Error(
+        private val message: String
+    ) : WeatherDomain {
+
+        override fun <T : Any> map(mapper: Mapper<T>) = mapper.mapError(message = message)
+    }
+}
