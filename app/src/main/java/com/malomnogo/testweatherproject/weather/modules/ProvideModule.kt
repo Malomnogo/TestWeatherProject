f27a42ba988c50d621c0fd0e88a17092d6032a14
Eugene <auricamercer@gmail.com>
2025-12-02 21:21:17 +0300

    feat: integrate weather module with cloud data source

--- app/src/main/java/com/malomnogo/testweatherproject/weather/modules/ProvideModule.kt
+++ app/src/main/java/com/malomnogo/testweatherproject/weather/modules/ProvideModule.kt
@@ -2,6 +2,7 @@ package com.malomnogo.testweatherproject.weather.modules
 
 import androidx.lifecycle.ViewModel
 import com.malomnogo.presentation.weather.WeatherViewModel
+import com.malomnogo.testweatherproject.weather.ProvideInstance
 
 interface ProvideModule {
 
@@ -9,12 +10,16 @@ interface ProvideModule {
 
     @Suppress("UNCHECKED_CAST")
     class Base(
-        private val core: Core
+        private val core: Core,
+        private val provideInstance: ProvideInstance
     ) : ProvideModule {
 
         override fun <T : ViewModel> module(clazz: Class<T>): Module<T> {
             return when (clazz) {
-                WeatherViewModel::class.java -> WeatherModule(core)
+                WeatherViewModel::class.java -> WeatherModule(
+                    core = core,
+                    provideInstance = provideInstance
+                )
                 else -> throw IllegalStateException("unknown viewModel $clazz")
             } as Module<T>
         }
