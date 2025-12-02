c40f9d2237dde11f8fa3fe0bbaf3523611a658c2
Eugene <auricamercer@gmail.com>
2025-12-02 22:15:59 +0300

    feat: add configuration change test with recreate

--- app/src/androidTest/java/com/malomnogo/testweatherproject/ScenarioTest.kt
+++ app/src/androidTest/java/com/malomnogo/testweatherproject/ScenarioTest.kt
@@ -13,15 +13,6 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ScenarioTest {
 
-    /**
-     * ⚠️ Before running the tests, you must enable the mock Application.
-     * Open `AndroidManifest.xml` and replace:
-     * `android:name=".weather.Release"`
-     * with
-     * `android:name=".weather.Mock"`
-     * Without this change the tests will not run correctly.
-     */
-
     @get:Rule
     var activityScenarioRule: ActivityScenarioRule<MainActivity> =
         ActivityScenarioRule(MainActivity::class.java)
@@ -36,8 +27,23 @@ class ScenarioTest {
         progressPage.checkNotVisible()
         weatherPage.checkNotVisible()
 
+        activityScenarioRule.scenario.recreate()
+
+        errorPage.checkError(message = "No internet connection")
+        progressPage.checkNotVisible()
+        weatherPage.checkNotVisible()
+
         errorPage.clickRetry()
         errorPage.checkNotVisible()
+
+        weatherPage.checkCurrentWeather(
+            city = "Moscow",
+            temperature = "30°C"
+        )
+        progressPage.checkNotVisible()
+        errorPage.checkNotVisible()
+
+        activityScenarioRule.scenario.recreate()
 
         weatherPage.checkCurrentWeather(
             city = "Moscow",
