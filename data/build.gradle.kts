49e413fb3e21963bee37d3441835d8adf12af971
Eugene <auricamercer@gmail.com>
2025-12-02 22:15:57 +0300

    feat: add test dependencies for data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -13,4 +13,7 @@ dependencies {
     api(libs.retrofit)
     api(libs.retrofit.converter.gson)
     api(libs.okhttp.logging.interceptor)
+
+    testImplementation(libs.junit)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
 }
