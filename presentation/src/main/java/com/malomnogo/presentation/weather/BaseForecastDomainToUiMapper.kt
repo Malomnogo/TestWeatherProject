140fa7e6511fb86ccdbae57bc84cfa8b80cbe98e
Eugene <auricamercer@gmail.com>
2025-12-03 14:35:21 +0300

    refactor: consolidate ForecastUiState with nested Hour and Day interfaces

--- presentation/src/main/java/com/malomnogo/presentation/weather/BaseForecastDomainToUiMapper.kt
+++ presentation/src/main/java/com/malomnogo/presentation/weather/BaseForecastDomainToUiMapper.kt
@@ -2,21 +2,14 @@ package com.malomnogo.presentation.weather
 
 import com.malomnogo.domain.ForecastDayDomain
 import com.malomnogo.domain.ForecastDomain
-import com.malomnogo.domain.HourDomain
 import com.malomnogo.presentation.core.FormatDate
 
 class BaseForecastDomainToUiMapper(
-    private val forecastDayMapper: BaseForecastDayDomainMapper,
-    private val hourMapper: HourDomain.Mapper<HourUiState>,
-    private val dayMapper: com.malomnogo.domain.DayDomain.Mapper<DayUiState>,
-    private val formatDate: FormatDate
+    private val forecastDayMapper: ForecastDayDomain.Mapper<ForecastDayUiState>
 ) : ForecastDomain.Mapper<ForecastUiState> {
 
     override fun mapSuccess(forecastDay: List<ForecastDayDomain>) = ForecastUiState.Base(
-        forecastDay = forecastDay.map { it.map(forecastDayMapper) },
-        hourMapper = hourMapper,
-        dayMapper = dayMapper,
-        formatDate = formatDate
+        forecastDay = forecastDay.map { it.map(forecastDayMapper) }
     )
 
     override fun mapError(message: String) = ForecastUiState.Empty
--- presentation/src/main/java/com/malomnogo/presentation/weather/ForecastDayUiState.kt
+++ presentation/src/main/java/com/malomnogo/presentation/weather/ForecastDayUiState.kt
@@ -1,12 +1,48 @@
 package com.malomnogo.presentation.weather
 
+import com.malomnogo.presentation.weather.views.ShowDay
+import com.malomnogo.presentation.weather.views.ShowHourForecast
+
 interface ForecastDayUiState {
+
+    fun update(dayView: ShowDay)
+    
+    fun updateHour(hourView: ShowHourForecast)
+    
+    fun toDayForecastUiState(): DayForecastUiState
 
     data class Success(
         val dateEpoch: String,
         val day: DayUiState,
         val hour: List<HourUiState>
-    ) : ForecastDayUiState
+    ) : ForecastDayUiState {
 
-    data object Error : ForecastDayUiState
+        override fun update(dayView: ShowDay) {
+            dayView.showDate(dateEpoch)
+            day.update(dayView)
+            dayView.change(true)
+        }
+
+        override fun updateHour(hourView: ShowHourForecast) {
+            hourView.showHourlyForecast(hour)
+            hourView.change(true)
+        }
+
+        override fun toDayForecastUiState() = DayForecastUiState.Base(
+            date = dateEpoch,
+            temperature = day
+        )
+    }
+
+    data object Error : ForecastDayUiState {
+        override fun update(dayView: ShowDay) {
+            dayView.change(false)
+        }
+
+        override fun updateHour(hourView: ShowHourForecast) {
+            hourView.change(false)
+        }
+
+        override fun toDayForecastUiState() = DayForecastUiState.Empty
+    }
 }
