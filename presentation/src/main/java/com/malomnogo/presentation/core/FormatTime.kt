c8aba9461905ecb3e9ade844ecaf38da9bcf698f
Eugene <auricamercer@gmail.com>
2025-12-03 14:35:16 +0300

    feat: add FormatTime interface for time formatting

--- presentation/src/main/java/com/malomnogo/presentation/core/FormatTime.kt
+++ presentation/src/main/java/com/malomnogo/presentation/core/FormatTime.kt
@@ -5,6 +5,7 @@ interface FormatTime {
     fun formatTime(timeEpoch: Long): String
 
     class Base : FormatTime {
+
         override fun formatTime(timeEpoch: Long): String {
             val date = java.util.Date(timeEpoch * 1000)
             val format = java.text.SimpleDateFormat("HH:mm", java.util.Locale.getDefault())
