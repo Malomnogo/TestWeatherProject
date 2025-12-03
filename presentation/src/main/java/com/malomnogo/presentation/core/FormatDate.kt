9094e2e2f7ca0519e4cb65050f28adf0638839b7
Eugene <auricamercer@gmail.com>
2025-12-03 14:35:17 +0300

    fix: remove formatTime from FormatDate interface

--- presentation/src/main/java/com/malomnogo/presentation/core/FormatDate.kt
+++ presentation/src/main/java/com/malomnogo/presentation/core/FormatDate.kt
@@ -5,6 +5,7 @@ interface FormatDate {
     fun formatDate(dateEpoch: Long): String
 
     class Base : FormatDate {
+
         override fun formatDate(dateEpoch: Long): String {
             val date = java.util.Date(dateEpoch * 1000)
             val format = java.text.SimpleDateFormat("d.M.yyyy", java.util.Locale.getDefault())
